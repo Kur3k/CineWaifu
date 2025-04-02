d18fec2e622b0d6ad7485b67d41b19e5c993f372
Kur3k <kur4k@proton.me>
2025-04-02 17:26:13 +0200

    Added darker / lighter shader, fixed order of generated frames.

--- CineWaifu.Domain/Model/RgbColor.cs
+++ CineWaifu.Domain/Model/RgbColor.cs
@@ -1,4 +1,17 @@
 ﻿namespace CineWaifu.Domain.Model
 {
-    public record RgbColor(int R, int G, int B);
+    public record RgbColor
+    {
+        public RgbColor(int R, int G, int B)
+        {
+            this.R = R < 0 ? 0 : R > 255 ? 255 : R;
+            this.G = G < 0 ? 0 : G > 255 ? 255 : G;
+            this.B = B < 0 ? 0 : B > 255 ? 255 : B;
+        }
+
+        public int R { get; init; }
+        public int G { get; init; }
+        public int B { get; init; }
+
+    }
 }
