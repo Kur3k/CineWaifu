dd3f0aca1a731d9099ffe43a7ac3fb245761b8ea
Kur3k <37422706+Kur3k@users.noreply.github.com>
2025-04-04 03:28:54 +0200

    Basic tcp server implementation. (#31)
    Co-authored-by: Kur3k

---
+++ CineWaifu.Application/Commands/ServerCommand.cs
@@ -0,0 +1,12 @@
+﻿using CineWaifu.Server;
+
+namespace CineWaifu.Application.Commands
+{
+    public class ServerCommand
+    {
+        public void Serve(string inputAnsiFileLocation, int port)
+        {
+            new CineWaifuServer(inputAnsiFileLocation, port).Run();
+        }
+    }
+}
---
+++ CineWaifu.Server/CineWaifuServer.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using K4os.Compression.LZ4.Streams;
+using System.Text;
+
+namespace CineWaifu.Server
+{
+    public class CineWaifuServer
+    {
+        public CineWaifuServer(string ansiFileLocation, int port)
+        {
+            _port = port;
+            _ansiFileLocation = ansiFileLocation;
+        }
+
+        public void Run()
+        {
+            string ipAddress = "127.0.0.1";
+
+            TcpListener server = new TcpListener(IPAddress.Parse(ipAddress), _port);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine($"Server listening on {ipAddress}:{_port}...");
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    string clientIP = ((IPEndPoint)client.Client.RemoteEndPoint!).Address.ToString();
+                    Console.WriteLine($"Client {clientIP} connected.");
+
+                    NetworkStream stream = client.GetStream();
+
+                    try
+                    {
+                        using (var uncompressed = LZ4Stream.Decode(File.OpenRead(_ansiFileLocation)))
+                        {
+                            using (StreamReader reader = new StreamReader(uncompressed))
+                            {
+                                while (!reader.EndOfStream)
+                                {
+                                    byte[] decodedFrame = Encoding.UTF8.GetBytes(reader.ReadLine()!);
+                                    stream.Write(decodedFrame);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Client exception: " + ex.Message);
+                    }
+                    finally
+                    {
+                        client.Close();
+                        Console.WriteLine($"Client {clientIP} disconnected.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private readonly int _port;
+        private readonly string _ansiFileLocation;
+    }
+}
